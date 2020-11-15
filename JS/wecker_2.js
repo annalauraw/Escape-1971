50220258a930c0868b6f217c873c9231b05bb629
Annabelle Wiegart <annabelle.wiegart@gmx.ch>
2020-11-15 19:48:09 +0100

    Grosser Wecker mit SVG-Ziffernblatt wird bei Klick angezeigt

--- JS/wecker_2.js
+++ JS/wecker_2.js
@@ -1,21 +1,25 @@
 // Wenn der Wecker geklickt wird, wird diese Funktion aufgerufen
-// Sie bewirkt, dass das Placeholder-Rätsel angezeigt wird
-function showPuzzle() {
-  var puzzle1 = document.getElementById("puzzle1");
-  puzzle1.classList.toggle("display");
+// Sie bewirkt, dass der Wecker in gross angezeigt wird
+function showClock() {
+  var clockbox = document.getElementById("clockbox");
+  var wecker_vorne = document.getElementById("wecker_vorne");
+  clockbox.classList.toggle("display");
+  wecker_vorne.classList.toggle("display");
 }
+
+function setup() {
+  var wecker = document.getElementById("wecker");
+  wecker.addEventListener("click", showClock);
+}
+
+window.addEventListener("load", setup);
+
 
 // Diese Funktion vergleicht den im Placeholder-Rätsel-Formular eingegebenen
-// Wert (solution1) mit dem Lösungswort 
-function checkPuzzle1() {
-  var solution1 = document.forms["puzzle1"]["solution1"].value;
-  if (solution1 == "1937") {
-    window.alert("Glückwunsch! Diese Antwort ist richtig!");
-  }
-}
-
-// function setup() {
-//   document.addEventListener([myEvent], [myFunction]);
+// Wert (solution1) mit dem Lösungswort
+// function checkPuzzle1() {
+  // var solution1 = document.forms["puzzle1"]["solution1"].value;
+  // if (solution1 == "1937") {
+  //   window.alert("Glückwunsch! Diese Antwort ist richtig!");
+  // }
 // }
-
-// window.addEventListener("load", setup);
