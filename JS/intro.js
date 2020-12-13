69cd9ed5833977de9088c44363f1d4f02dccee0b
Annabelle Wiegart <annabelle.wiegart@gmx.ch>
2020-12-13 17:44:58 +0100

    Rätsel mit Fragen zum Video (Lotti Ruckstuhl)

--- JS/intro.js
+++ JS/intro.js
@@ -1,22 +1,28 @@
 // Knopf an Fernseher, um Testvideo zu starten
 var button_lotti = document.getElementById("button_lotti");
+// iFrame (später hoffentlich Videodatei)
 var lotti = document.getElementById("lotti");
 var testvideo_2 = document.getElementById("testvideo_2");
 var lotti_isDisplayed = false;
 var lotti_isPlaying = false;
 var testvideo_2_isDisplayed = false;
 var testvideo_2_isPlaying = false;
-
+// Button "Ich bin informiert", startet das Rätsel zum Video
+var button_startPuzzle = document.getElementById("startPuzzle");
+// div mit Rätsel zum Video
+var puzzle_videoQuestions = document.getElementById("puzzle_videoQuestions");
+// Rätsel zum Video, Formular mit Fragen und Eingabefeldern
+var form_videoQuestions = document.forms["puzzle_videoQuestions"];
 
 // Wenn der oberste Knopf am Fernseher geklickt wird, wird
 // das Testvideo gestartet
 
 // Testfunktion, um die Image map zu sehen (wo ist der klickbare Kreis?)
-function hoverOverButton() {
-  button_testvideo_2.style.cursor = "pointer";
-}
+// function hoverOverButton() {
+//   button_testvideo_2.style.cursor = "pointer";
+// }
 
-// Die Funktionen playTestvideo_1 und playTestvideo_2 schreien
+// Die Funktionen playLotti und playTestvideo_2 schreien
 // nach einer Klasse
 
 function playLotti() {
@@ -69,9 +75,26 @@ function playTestvideo_2() {
   }
 }
 
+function startPuzzle() {
+  puzzle_videoQuestions.classList.toggle("display");
+}
+
+function checkPuzzle() {
+  // Was die Spielerin eingegeben hat
+  var solution1 = document.forms["puzzle_videoQuestions"]["solution1"].value;
+  var solution2 = document.forms["puzzle_videoQuestions"]["solution2"].value;
+  var solution3 = document.forms["puzzle_videoQuestions"]["solution3"].value;
+  // Hier evtl mit RegEx arbeiten, damit Varianten als richtig erkannt werden
+  if (solution1 == "1953" && solution2 == "Waadt, Genf" && solution3 == "Unterbäch") {
+    window.alert("Glückwunsch! Diese Antworten sind richtig!");
+  }
+}
+
 function setup() {
   button_lotti.addEventListener("click", playLotti);
   button_testvideo_2.addEventListener("click", playTestvideo_2);
+  button_startPuzzle.addEventListener("click", startPuzzle);
+  form_videoQuestions.addEventListener("submit", checkPuzzle);
   // button_testvideo_2.addEventListener("mouseover", hoverOverButton);
 }
 
