3646a59e59fc19e6e6ece3207ce7863b57122d1a
nmeier2 <nicole_meier2@hotmail.com>
2021-01-31 14:49:24 +0100

    Bundesplatz durchspielbar

--- JS/bundesplatz.js
+++ JS/bundesplatz.js
@@ -1,11 +1,4 @@
-// Button "Rätsel starten"
-var button_startPuzzle = document.getElementById("startPuzzle");
-// Button, der die Antworten zu den Lieberherr-Fragen "abschickt"
-var puzzleLieberherrButton = document.getElementById("puzzleLieberherrButton");
-// div mit Rätsel zum Video
-var puzzle_lieberherrQuestions = document.getElementById("puzzle_lieberherrQuestions");
-// Rätsel zum Video, Formular mit Fragen und Eingabefeldern
-var form_lieberherrQuestions = document.forms["puzzle_LieberherrQuestions"];
+
 // div mit Hintergrundbild - wird für das Erscheinen bzw. Bewegen der Fahne
 // gebraucht
 var hintergrund = document.getElementById("hintergrund");
@@ -13,14 +6,31 @@ var hintergrund = document.getElementById("hintergrund");
 var flag = document.getElementById("fahne");
 // Button am Fernseher, der das Lieberherr-Video startet
 // Noch passiert nichts, wenn man klickt
-var button_lieberherr = document.getElementById("button_lieberherr");
+
 // Helvetias Hand
 var helvetia = document.getElementById("helvetia");
+
+var fernseher =document.getElementById("fernseher");
+var puzzleVertrauen = document.getElementById("puzzleVertrauen");
+
+var showTV= document.getElementById("showTV");
+var SRF_player=document.getElementById("SRF_player");
+
+var player = SRG.PlayerManager.createPlayer('SRF_player','inline','urn:srf:video:280726b6-f954-4859-ab4e-503aab00d3a5');
+var playbackInterval = undefined;
+var player_isDisplayed = false;
+
+function richtig(){
+	puzzleVertrauen.classList.toggle("display");
+	hintergrund.style.opacity="1";
+	callFlag();
+}
 
 // Funktion, die das Rätsel zu Lieberherr anzeigt
 function startPuzzle() {
   puzzle_lieberherrQuestions.classList.toggle("display");
 }
+
 
 // Funktion, die die Fahne bewegt
 function moveFlag(event) {
@@ -39,8 +49,12 @@ function moveFlag(event) {
 // Wenn Fahne an den richtigen Koordinaten ist: Helvetia in die Hand drücken
 function putFlag() {
   document.removeEventListener("mousemove", moveFlag);
-  flag.style.left = "662.5px";
-  flag.style.top = "-4px";
+  flag.style.left = "570px";
+  flag.style.top = "150px";
+  document.getElementById('Tor').play();
+  document.getElementById("Tor").addEventListener('ended', function(){
+	  location.assign("nationalratssaal.html");
+  });
 }
 
 // Funktion, die die Fahne erscheinen lässt: Es wird ein addEventListener
@@ -52,29 +66,161 @@ function callFlag() {
   // button_removeFlag.addEventListener("click", stopFlag);
 }
 
-function checkPuzzle() {
-  // Was die Spielerin eingegeben hat
-  var solution1 = document.forms["puzzle_lieberherrQuestions"]["solution1"].value;
-  // var solution2 = document.forms["puzzle_videoQuestions"]["solution2"].value;
-  // var solution3 = document.forms["puzzle_videoQuestions"]["solution3"].value;
-  // Hier evtl mit RegEx arbeiten, damit Varianten als richtig erkannt werden
-  if (solution1 == "Händ Vertrauen mit den Frauen!") {
-    // window.alert("Glückwunsch! Diese Antwort ist richtig!");
-    puzzle_lieberherrQuestions.classList.toggle("display");
-    callFlag();
-  }
-}
+
 
 // Testfunktion, um eine Image map zu sehen (wo ist der klickbare Kreis?)
-// function showArea(area) {
+//function showArea(area) {
 //   area.style.cursor = "pointer";
 // }
 
+// Objekt, das den Zustand der Videos kennt - sind sie im Player geladen?
+var loadState = {
+  lieberherrV: true
+}
+
+// Objekt, das den Zustand der Videos kennt - werden sie gerade abgespielt?
+var playState = {
+  lieberherrV: false
+}
+
+// Funktion, die eine Closure enthält und die Callback-Funktion für das
+// Intervall zurückgibt,
+// die an das Video-Objekt gebunden ist (andernfalls wäre sie an
+// Window gebunden)
+
+function puzzleVertrauenFrauen(){
+	fernseher.classList.toggle("display");
+	SRF_player.classList.toggle("display");
+	puzzleVertrauen.classList.toggle("display");
+}
+
+
+function returnCheckPlaybackTime(obj) {
+  return function() {
+    player.getCurrentTime(function (currentTime) {
+      if (currentTime >= obj.stopTime) {
+        obj.pause();
+        obj.stopInterval();
+		puzzleVertrauenFrauen();
+        player.seek(obj.startTime);
+        playState[obj.name] = false;
+      }
+    });
+  }
+}
+
+// Prototyp SRF_Video
+function SRF_Video(name, urn, startTime, stopTime) {
+  // startTime and stopTime in seconds (int or float)
+  // var self = this;
+  this.name = name;
+  this.urn = urn;
+  this.startTime = startTime;
+  this.stopTime = stopTime;
+  this.fullUrn = urn + '&start=' + startTime;
+  //this.subtitles = subtitles;
+}
+
+SRF_Video.prototype = {
+
+  /*showSubtitles: function() {
+    this.subtitles.classList.toggle("display");
+  },*/
+
+  play: function() {
+    player.play();
+    //area_hideTV.removeEventListener("click", hideTV);
+    playState[this.name] = true;
+    if (player_isDisplayed == false) {
+      this.displayPlayer();
+    }
+    //this.subtitles.classList.toggle("display");
+    this.startInterval();
+    //showSubtitles();
+    // if (black.classList.contains("display")) {
+    //   black.classList.toggle("display");
+    // }
+  },
+
+  pause: function() {
+    player.pause();
+    //this.showSubtitles();
+    playState[this.name] = false;
+    //area_hideTV.addEventListener("click", hideTV);
+  },
+
+  recreatePlayer: function() {
+    // if (black.classList.contains("display") == false) {
+    //   black.classList.toggle("display");
+    // }
+    this.stopInterval();
+    player.destroy();
+    this.resetLoadState();
+    this.resetPlayState();
+    player = SRG.PlayerManager.createPlayer('SRF_player','inline', this.fullUrn);
+    loadState[this.name] = true;
+  },
+
+  resetLoadState: function() {
+    loadState.lieberherrV = false;
+  },
+
+  resetPlayState: function() {
+    playState.lieberherrV = false;
+  },
+
+  startInterval: function() {
+    clearInterval(playbackInterval);
+    // Der Variable wird eine Funktion ausserhalb des Objekts zugewiesen,
+    // die eine Closure enthält und die Callback-Funktion zurückgibt,
+    // die an das Video-Objekt gebunden ist (andernfalls wäre sie an
+    // Window gebunden)
+    let checkPlaybackTime = returnCheckPlaybackTime(this);
+    playbackInterval = setInterval(checkPlaybackTime, 1000);
+  },
+
+  stopInterval: function() {
+    if (playbackInterval != undefined) {
+      clearInterval(playbackInterval);
+      // playbackInterval = undefined;
+    }
+  },
+
+  displayPlayer: function() {
+    SRF_player.classList.toggle("display");
+	fernseher.classList.toggle("display");
+	hintergrund.style.opacity="0.2";
+    player_isDisplayed = true;
+  },
+
+  handle: function() {
+    // Wenn das Video schon geladen ist, Player starten
+    if (loadState[this.name] == true) {
+      if (playState[this.name] == false) {
+        this.play();
+      }
+      else {
+        this.pause();
+      }
+    }
+    // Sonst Video laden
+    else {
+      this.recreatePlayer();
+      this.play();
+    }
+  },
+}
+
+
+lieberherrV = new SRF_Video('lieberherrV', 'urn:srf:video:280726b6-f954-4859-ab4e-503aab00d3a5', 0, 5);
+
+
 function setup() {
-  button_startPuzzle.addEventListener("click", startPuzzle);
-  puzzleLieberherrButton.addEventListener("click", checkPuzzle);
-  // button_lieberherr.addEventListener("mouseover", function(event) {showArea(event.target)});
-  // helvetia.addEventListener("mouseover", function(event) {showArea(event.target)});
+  //button_startPuzzle.addEventListener("click", startPuzzle);
+  //puzzleLieberherrButton.addEventListener("click", checkPuzzle);
+  //button_lieberherr.addEventListener("mouseover", function(event) {showArea(event.target)});
+  //helvetia.addEventListener("mouseover", function(event) {showArea(event.target)});
+  showTV.addEventListener("click", lieberherrV.handle.bind(lieberherrV));
 }
 
 window.addEventListener("load", setup);
