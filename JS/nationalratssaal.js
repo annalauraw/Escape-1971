9dc4b980035b1b655bcd60890749b6c2f0999444
nmeier2 <nicole_meier2@hotmail.com>
2021-02-05 12:33:51 +0100

    Bilder gehen bei drag mit

--- JS/nationalratssaal.js
+++ JS/nationalratssaal.js
@@ -4,6 +4,7 @@ var pult=document.getElementById("pult");
 var abfall=document.getElementById("abfall");
 var hintergrund=document.getElementById("hintergrund");
 var istargumentoffen=null;
+var crt="";
 
 for (i=1; i<11; i++){
 	this ["arg"+i]=document.getElementById("arg"+i);
@@ -23,6 +24,7 @@ function allowDrop(ev){
 
 function checkElements(number){
 	var check=document.getElementsByClassName("argument");
+	console.log(check);
 	
 	if (check.length<1){
 		if (number==2){
@@ -39,7 +41,8 @@ function checkElements(number){
 	
 }
 
-function dropAbfall(){
+function dropAbfall(event){
+	event.preventDefault();
 	
 	if (test < 6){
 		//alert("Korrekt");
@@ -50,7 +53,7 @@ function dropAbfall(){
 	}
 }
 function dropPult(){
-	
+	event.preventDefault();
 	if (test > 5){
 		//alert("Korrekt");
 		document.getElementById('AV-applaus').play();
@@ -61,20 +64,60 @@ function dropPult(){
 	
 }
 
-function dragArgument(event){
-	//console.log("YES");
-	//argument=event.target.id;
+function moveCrt(event){
+	console.log("move");
 	
-	//ev.dataTransfer.setData("text", "junk");
-	var img = document.createElement("img");
-    img.src = "Bilder/bilder/"+argument+".png";
-    event.dataTransfer.setDragImage(img, 200, 200);
-
+	var windowMarginLeft = (window.innerWidth - 1200) / 2;
+  crt.style.left = event.clientX - windowMarginLeft + 20 + 'px';
+  crt.style.top = event.clientY -50 + 'px';
+	
+	/*dragX = event.pageX; 
+	dragY = event.pageY;
+    crt.style.left= dragX+"px";
+	crt.style.top=  dragY+"px";
+    console.log("X: "+dragX+" Y: "+dragY);*/
 }
+
+
+
+function dragstartArgument(event){
+	//argumentgross.classList.toggle("display");
+	argumentgross.style.opacity="0";
+	var img = document.createElement("img");
+    //img.src = "Bilder/bilder/"+argument+".png";
+	img.src="";
+    event.dataTransfer.setDragImage(img, 200, 200);
+	//crt = event.target.cloneNode(true);
+	crt = document.createElement("img");
+	crt.id="crt";
+	crt.src = "Bilder/bilder/"+argument+".png";
+    //crt.style.position = "absolute"; 
+	
+	document.getElementById("imagecontainer").appendChild(crt);
+	event.dataTransfer.setData('text/html', null);
+	
+	
+}
+
+function dragArgument(event){
+	event.preventDefault();
+	//event.dataTransfer.setData('text/html', null);
+}
+
+
+ function bildweg (event) {
+	document.getElementById("crt").remove();
+	//argumentgross.classList.toggle("display");
+	argumentgross.style.opacity="1";
+	 
+}
+
+
 function closeArgument(){
 	console.log("YES");
 	hintergrund.removeEventListener("click", closeArgument);
 	argumentgross.classList.toggle("display");
+	
 	istargumentoffen=null;
 	if (document.getElementById(argument)){
 	document.getElementById(argument).classList.toggle("hide");
@@ -104,8 +147,8 @@ function openArgument(event){
 	hintergrund.addEventListener("click", closeArgument);
 	document.getElementById(argument).classList.toggle("hide");
 	
-	abfall.setAttribute("ondrop", "dropAbfall()")
-	pult.setAttribute("ondrop", "dropPult()")
+	abfall.setAttribute("ondrop", "dropAbfall(event)")
+	pult.setAttribute("ondrop", "dropPult(event)")
 	istargumentoffen=1;
 }
 
@@ -113,11 +156,16 @@ function openArgument(event){
 
 function setup() {
 	
+	document.addEventListener("dragover", moveCrt);
+	
    for (i=1; i<11; i++){
 	rad="arg"+i 
 		document.getElementById(rad).addEventListener("click", openArgument);
+		
 	
 }
 }
+
+
 
 window.addEventListener("load", setup);
