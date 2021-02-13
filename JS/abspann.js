5345f06df3f85b3e912d27ae1b4b305c78ae9d63
Annabelle Wiegart <annabelle.wiegart@gmx.ch>
2021-02-13 20:50:26 +0100

    Abspann mit Textkorrekturen, CC-Lizenz und eingebettetem Soundcloud-Player

--- JS/abspann.js
+++ JS/abspann.js
@@ -2,20 +2,24 @@ var song = document.getElementById("song");
 var button_animation = document.getElementById("animation");
 var content = document.getElementsByClassName("imagecontainer")[0];
 var button_song = document.getElementById("button_song");
+var buttons_top = document.getElementById("buttons_top");
 
 function backToStart() {
   setTimeout(function() {
     content.classList.toggle("end");
     content.removeEventListener("animationend", backToStart);
     content.removeEventListener("webkitAnimationEnd", backToStart);
+    buttons_top.classList.toggle("display");
   }, 3000);
 }
 
 function startAnimation() {
-  song.play();
-  content.classList.toggle("animate");
-  content.addEventListener("animationend", backToStart);
-  content.addEventListener("webkitAnimationEnd", backToStart);
+  setTimeout(function() {
+    song.play();
+    content.classList.toggle("animate");
+    content.addEventListener("animationend", backToStart);
+    content.addEventListener("webkitAnimationEnd", backToStart);
+  }, 3000);
 }
 
 function setup() {
